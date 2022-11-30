b1a6a6e43a90f95ea6b6d86bd9114eb64566a529
Fullfifax <fullfifax1245@gmail.com>
2022-11-30 09:22:33 +0000

    add drum scripts

---
+++ 1_Js_Kidondonta/assets/js/app.js
@@ -0,0 +1,22 @@
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    // get key pressed
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // get div of key pressed
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+    if(!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0
+    audio.play();
+}
+
+// get all keys
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
